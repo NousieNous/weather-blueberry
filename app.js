dbb2d4626fa5732ce4f97ae31365ea0a20c8b20f
NousieNous <132939272+NousieNous@users.noreply.github.com>
2023-09-16 19:09:59 +0100

    worked on index days

--- app.js
+++ app.js
@@ -9,15 +9,7 @@ function formatDate(timestamp) {
     minutes = `0${minutes}`;
   }
 
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
@@ -35,27 +27,32 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  forecast.forEach(function (forecastDay) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
       <div class="col-2">
-        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <div class="weather-forecast-date">${formatDate(
+          forecastDay.time * 1000
+        )}</div>
+        ${index}
         <img
           src="${forecastDay.condition.icon_url}"
           alt=""
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${
+          <span class="weather-forecast-temperature-max"> ${Math.round(
             forecastDay.temperature.maximum
-          } </span>
-          <span class="weather-forecast-temperature-min"> ${
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
             forecastDay.temperature.minimum
-          } </span>
+          )}° </span>
         </div>
       </div>
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
